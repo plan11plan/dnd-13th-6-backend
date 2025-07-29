dff4e770f89395fc34913c1d8f6492082849b456
plan11plan <tjtfy7028@gmail.com>
2025-07-29 17:00:52 +0900

    chore: 프로젝트 초기화
    - Spring Boot 프로젝트 생성
      - Group: com.dnd13
      - Artifact: runners-server
      - Package: com.dnd13.runners
      - Java 24, Gradle 기반
    
    - 의존성 추가 (build.gradle):
      - Web, Data JPA, Validation
      - Lombok
      - SpringDoc Swagger (v2.7.0)
      - JWT (jjwt 0.12.6)
        - core, impl, jackson 모듈 분리 적용
      - DB Driver
        - H2 (테스트용), MySQL
      - 테스트 의존성
        - spring-boot-starter-test
        - junit-platform-launcher

---
+++ src/main/java/com/dnd13/runners_server/RunnersServerApplication.java
@@ -0,0 +1,13 @@
+package com.dnd13.runners_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunnersServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunnersServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dnd13/runners_server/RunnersServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dnd13.runners_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnersServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
