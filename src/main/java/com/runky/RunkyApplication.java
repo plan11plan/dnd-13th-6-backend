13308884452555ea56b97f9b80fbdb3e6754f92c
hgh1472 <hgh1472@naver.com>
2025-08-09 20:03:30 +0900

    chore: 프로젝트 기본 설정 수정 (#5)

--- src/main/java/com/runky/RunkyApplication.java
+++ src/main/java/com/runky/RunkyApplication.java
@@ -1,12 +1,22 @@
 package com.runky;
 
+import jakarta.annotation.PostConstruct;
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class RunkyApplication {
+
+    @PostConstruct
+    public void started() {
+        // set timezone
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(RunkyApplication.class, args);
--- src/main/java/com/runky/global/entity/BaseTimeEntity.java
+++ src/main/java/com/runky/global/entity/BaseTimeEntity.java
@@ -1,35 +1,69 @@
 package com.runky.global.entity;
 
-import java.time.LocalDateTime;
-
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import java.time.ZonedDateTime;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
-@AllArgsConstructor(access = AccessLevel.PROTECTED)
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @EntityListeners(value = {AuditingEntityListener.class})
 public abstract class BaseTimeEntity {
 
-	@CreatedDate
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-	@Column(columnDefinition = "TIMESTAMP", nullable = false, updatable = false)
-	private LocalDateTime createdAt;
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
 
-	@LastModifiedDate
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-	@Column(columnDefinition = "TIMESTAMP", nullable = false)
-	private LocalDateTime updatedAt;
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
 
+    @Column(name = "deleted_at")
+    private ZonedDateTime deletedAt;
+
+    /**
+     * 엔티티의 유효성을 검증한다. 이 메소드는 PrePersist 및 PreUpdate 시점에 호출된다.
+     */
+    protected void guard() {
+    }
+
+    @PrePersist
+    private void prePersist() {
+        guard();
+
+        ZonedDateTime now = ZonedDateTime.now();
+        this.createdAt = now;
+        this.updatedAt = now;
+    }
+
+    @PreUpdate
+    private void preUpdate() {
+        guard();
+
+        this.updatedAt = ZonedDateTime.now();
+    }
+
+    /**
+     * delete 연산은 멱등하게 동작할 수 있도록 한다. (삭제된 엔티티를 다시 삭제해도 동일한 결과가 나오도록)
+     */
+    public void delete() {
+        if (this.deletedAt == null) {
+            this.deletedAt = ZonedDateTime.now();
+        }
+    }
+
+    /**
+     * restore 연산은 멱등하게 동작할 수 있도록 한다. (삭제되지 않은 엔티티를 복원해도 동일한 결과가 나오도록)
+     */
+    public void restore() {
+        if (this.deletedAt != null) {
+            this.deletedAt = null;
+        }
+    }
 }
