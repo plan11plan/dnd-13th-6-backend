4efb5e579bd192522c7b3d8c4b655e61d3b448cb
hgh1472 <hgh1472@naver.com>
2025-08-12 02:36:05 +0900

    refactor: retry 처리 제거

--- src/main/java/com/runky/crew/domain/CrewService.java
+++ src/main/java/com/runky/crew/domain/CrewService.java
@@ -4,9 +4,6 @@ import com.runky.global.error.GlobalErrorCode;
 import com.runky.global.error.GlobalException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
-import org.springframework.retry.annotation.Backoff;
-import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -16,9 +13,6 @@ public class CrewService {
     private final CodeGenerator codeGenerator;
 
     @Transactional
-    @Retryable(retryFor = {ObjectOptimisticLockingFailureException.class, GlobalException.class},
-            maxAttempts = 3, backoff = @Backoff(delay = 100)
-    )
     public Crew create(CrewCommand.Create command) {
         CrewMemberCount crewMemberCount = crewRepository.findCountByMemberId(command.userId())
                 .orElseThrow(() -> new GlobalException(GlobalErrorCode.NOT_FOUND));
