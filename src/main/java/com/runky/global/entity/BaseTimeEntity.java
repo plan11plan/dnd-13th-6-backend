ed6cd29cdad4045e063e825c4d6829de1e274f43
hgh1472 <hgh1472@naver.com>
2025-08-12 01:13:29 +0900

    fix: ZonedDateTime과 LocalDateTime 충돌 제거

--- src/main/java/com/runky/global/entity/BaseTimeEntity.java
+++ src/main/java/com/runky/global/entity/BaseTimeEntity.java
@@ -7,8 +7,6 @@ import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import java.time.ZonedDateTime;
 import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
@@ -16,11 +14,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(value = {AuditingEntityListener.class})
 public abstract class BaseTimeEntity {
 
-    @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private ZonedDateTime createdAt;
 
-    @LastModifiedDate
     @Column(name = "updated_at", nullable = false)
     private ZonedDateTime updatedAt;
 
